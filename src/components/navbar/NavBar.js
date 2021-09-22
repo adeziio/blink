9736855bd6495592deff739d128d6bc9a7656be8
Aden <adeziio@yahoo.com>
2021-09-22 15:48:24 -0400

    update navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Menu, Header, Icon } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Header, Icon } from 'semantic-ui-react';
+import { Navbar, Nav } from 'react-bootstrap';
 import './NavBar.css';
 
 
@@ -24,63 +24,29 @@ export default class NavBar extends Component {
         activeItem = paths.some((item) => item === activeItem) ? activeItem : "/jennie";
 
         return (
-            <Menu
-                size="massive"
-                secondary
-                inverted
-                className="navbar-container"
-                stackable
-            >
-                <Menu.Item>
-                    <Header as='h2' inverted color="white">
-                        <Icon name='heart outline' />
-                        <Header.Content>
-                            BLIИK 블링크
-                            <Header.Subheader>BLΛƆKPIИK 블랙핑크</Header.Subheader>
-                        </Header.Content>
-                    </Header>
-                </Menu.Item>
-                <Menu.Item
-                    as={Link}
-                    to='/jennie'
-                    name='/jennie'
-                    active={activeItem === '/jennie'}
-                    onClick={this.handleItemClick}
-                    className="navbar-item"
-                >
-                    Jennie
-                </Menu.Item>
-                <Menu.Item
-                    as={Link}
-                    to='/jisoo'
-                    name='/jisoo'
-                    active={activeItem === '/jisoo'}
-                    onClick={this.handleItemClick}
-                    className="navbar-item"
-                >
-                    Jisoo
-                </Menu.Item>
-                <Menu.Item
-                    as={Link}
-                    to='/rose'
-                    name='/rose'
-                    active={activeItem === '/rose'}
-                    onClick={this.handleItemClick}
-                    className="navbar-item"
-                >
-                    Rosé
-                </Menu.Item>
-                <Menu.Item
-                    as={Link}
-                    to='/lisa'
-                    name='/lisa'
-                    active={activeItem === '/lisa'}
-                    onClick={this.handleItemClick}
-                    className="navbar-item"
-                >
-                    Lisa
-                </Menu.Item>
-            </Menu>
+            <>
+                <Navbar className="navbar-container" expand="lg" >
+                    <Navbar.Brand href="/jennie">
+                        <Header as='h2' inverted color="white">
+                            <Icon name='heart outline' />
+                            <Header.Content>
+                                BLIИK 블링크
+                                <Header.Subheader>BLΛƆKPIИK 블랙핑크</Header.Subheader>
+                            </Header.Content>
+                        </Header>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="navbar-item">
+                            <Nav.Link href="/jennie" className="navbar-item">Jennie</Nav.Link>
+                            <Nav.Link href="/jisoo" className="navbar-item">Jisoo</Nav.Link>
+                            <Nav.Link href="/rose" className="navbar-item">Rosé</Nav.Link>
+                            <Nav.Link href="/lisa" className="navbar-item">Lisa</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Navbar>
+            </>
         )
     }
 }
